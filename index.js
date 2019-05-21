bbc084aa64eb1e7ab1cb105e6f6392a0ac289700
Michele Saba <michelemsaba@gmail.com>
2019-05-21 06:56:50 -0500

    file setup for lab

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-'use strict';
+"use strict";
 
-const greet = require('./lib/greet.js');
-const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
-console.log(math.add(1,3)); // 4
-console.log(math.subtract(1,3)); // -2
+const greet = require("./lib/greet.js");
+const math = require("./lib/arithmetic.js");
+console.log(greet("JOHN"));
+console.log(math.add(1, 3)); // 4
+console.log(math.subtract(1, 3)); //-2
+console.log(math.multiply(4, 3)); // 12
+console.log(math.divide(15, 3)); // 5
