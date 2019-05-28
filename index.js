8424a32d7cc92fd206d47d3e69f925768019d473
Michele Saba <michelemsaba@gmail.com>
2019-05-28 12:19:44 -0500

    fixed eslint quotes issue on index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-const greet = require("./lib/greet.js");
-const math = require("./lib/arithmetic.js");
+const greet = require('./lib/greet.js');
+const math = require('./lib/arithmetic.js');
 
-console.log(greet("JOHN"));
-console.log(greet("Someone"));
+console.log(greet('JOHN'));
+console.log(greet('Someone'));
 console.log(greet());
 console.log(math.add(1, 3)); // 4
 console.log(math.subtract(1, 3)); //-2
