f2c2462c6f30a34978ae1bd28e420c6dc8aaf906
Michele Saba <michelemsaba@gmail.com>
2019-05-28 07:11:38 -0500

    fixed quotes for eslint

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict';
 
-const arithmetic = require("../lib/arithmetic.js");
+const arithmetic = require('../lib/arithmetic.js');
 
-describe("add", () => {
-  it("adds two numbers", () => {
+describe('add', () => {
+  it('adds two numbers', () => {
     const res = arithmetic.add(2, 4);
     expect(res).toBe(6);
   });
 });
-describe("subtracts", () => {
-  it("subtracts", () => {
+describe('subtracts', () => {
+  it('subtracts', () => {
     const res = arithmetic.subtract(2, 4);
     expect(res).toBe(-2);
   });
 });
-describe("multiply", () => {
-  it("multiplies two numbers", () => {
+describe('multiply', () => {
+  it('multiplies two numbers', () => {
     const res = arithmetic.multiply(2, 4);
     expect(res).toBe(8);
   });
 });
-describe("division", () => {
-  it("division", () => {
+describe('division', () => {
+  it('division', () => {
     const res = arithmetic.divide(12, 4);
     expect(res).toBe(3);
   });
