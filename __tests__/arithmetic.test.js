1013f1b075bbe2166b1b618caecc11e91f831e63
Michele Saba <michelemsaba@gmail.com>
2019-05-28 06:29:30 -0500

    fixed some eslint errors

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-const arithmetic = require('../lib/arithmetic.js');
+const arithmetic = require("../lib/arithmetic.js");
 
-describe('add', () => {
-    it('adds two numbers', () => {
-        const res = arithmetic.add(2, 4);
-        expect(res).toBe(6)
-    })
+describe("add", () => {
+  it("adds two numbers", () => {
+    const res = arithmetic.add(2, 4);
+    expect(res).toBe(6);
+  });
 });
-describe('subtracts', () => {
-    it('subtracts', () => {
-        const res = arithmetic.subtract(2, 4);
-        expect(res).toBe(-2)
-    })
+describe("subtracts", () => {
+  it("subtracts", () => {
+    const res = arithmetic.subtract(2, 4);
+    expect(res).toBe(-2);
+  });
 });
-describe('multiply', () => {
-    it('multiplies two numbers', () => {
-        const res = arithmetic.multiply(2, 4);
-        expect(res).toBe(8)
-    })
+describe("multiply", () => {
+  it("multiplies two numbers", () => {
+    const res = arithmetic.multiply(2, 4);
+    expect(res).toBe(8);
+  });
 });
-describe('division', () => {
-    it('division', () => {
-        const res = arithmetic.divide(12, 4);
-        expect(res).toBe(3)
-    })
+describe("division", () => {
+  it("division", () => {
+    const res = arithmetic.divide(12, 4);
+    expect(res).toBe(3);
+  });
 });
--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,17 +1,17 @@
-'use strict'
+'use strict';
 
-const greet = require("../lib/greet.js");
+const greet = require('../lib/greet.js');
 
 describe('greet', () => {
-    it('says hello', () => {
-        const res = greet('john');
-        expect(res).toBe('hello, john')
-    })
+  it('says hello', () => {
+    const res = greet('john');
+    expect(res).toBe('hello, john');
+  });
 });
 
 describe('greet', () => {
-    it('says hello name', () => {
-        const res = greet(name);
-        expect(res).toBe(`hello, ${name}`)
-    })
+  it('says hello name', () => {
+    const res = greet(name);
+    expect(res).toBe(`hello, ${name}`);
+  });
 });
